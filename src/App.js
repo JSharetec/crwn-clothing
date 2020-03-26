45239ad860b47b2206570a652773a55040493b79
Hamza Jorio <hamza.jorio@sharetec.ma>
2020-03-26 21:41:04 +0100

    First configuration Routing and use of HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-
+import {Route , Switch} from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 
+const HatPage = () => (
+  <div>
+  <h1> Hats Page</h1>
+  </div>
+)
+
 function App() {
   return (
-    <div >
-  <HomePage></HomePage>
-    </div>
+  //   <div >
+  // <HomePage></HomePage>
+  //   </div>
+  <div>
+  <Switch>
+  <Route exact path='/' component = {HomePage}></Route>
+  <Route  path='/shop/hats' component = {HatPage}></Route>
+  </Switch>
+ 
+  </div>
+ 
   );
 }
 
