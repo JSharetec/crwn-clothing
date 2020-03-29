0fa6b931fc0fde5ca73c19571dd1b76431d25752
Hamza Jorio <hamza.jorio@sharetec.ma>
2020-03-29 12:58:58 +0100

    Sign in component without persistance

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route , Switch} from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shoppage.component';
+import SignInSignOutPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component'
 import Header from './components/header/header.component'
 
 
@@ -15,6 +16,7 @@ function App() {
   <Header/>
   <Switch>
   <Route exact path='/' component = {HomePage}></Route>
+  <Route exact path='/signin' component = {SignInSignOutPage}></Route>
   <Route  path='/shop' component = {ShopPage}></Route>
   </Switch>
  
